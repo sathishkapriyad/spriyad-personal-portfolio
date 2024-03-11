80d01921914debabba5362f990b1948a147a3f11
Sathiska Priyad <spriyad.infor@gmail.com>
2024-03-11 23:52:58 +0000

    Update header hero section

--- app/(homes)/home-8/page.jsx
+++ app/(homes)/home-8/page.jsx
@@ -1,10 +1,11 @@
 import Footer7 from "@/components/footers/Footer7";
+import Header1 from "@/components/headers/Header1";
 import Header7 from "@/components/headers/Header7";
 import Blogs from "@/components/homes/home-8/Blogs";
 import Clients from "@/components/homes/home-8/Clients";
 import Cta from "@/components/homes/home-8/Cta";
 import Features from "@/components/homes/home-8/Features";
-import Hero from "@/components/homes/home-8/Hero";
+import Hero from "@/components/homes/home-4/Hero";
 import Projects from "@/components/homes/home-8/Projects";
 import Testimonials from "@/components/homes/home-8/Testimonials";
 import React from "react";
@@ -14,7 +15,7 @@ export const metadata = {
 export default function HomePage8() {
   return (
     <div>
-      <Header7 />
+      <Header1 />
       <Hero />
       <Features />
       <Projects />
--- components/homes/home-8/Hero.jsx
+++ components/homes/home-8/Hero.jsx
@@ -1,47 +1,70 @@
-"use client";
+import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React from "react";
 
 export default function Hero() {
-  useEffect(() => {
-    document.body.classList.add("bg-title");
-    return () => {
-      document.body.classList.remove("bg-title");
-    };
-  }, []);
-
   return (
-    <div
-      className="hero-wrapper hero-8 background-image"
-      id="hero"
-      style={{ backgroundImage: "url(/assets/img/hero/hero-8-1.png)" }}
-    >
-      <div className="container">
-        <div className="hero-style8">
-          <div className="row">
-            <div className="col-lg-8">
-              <h2 className="hero-subtitle wow img-custom-anim-left animated text-smoke">
-                Hi, I’m Daniel Pickman
-              </h2>
-              <h1 className="hero-title wow img-custom-anim-left animated text-smoke">
-                Senior Graphics Designer
-              </h1>
-            </div>
-            <div className="col-xl-4 col-lg-5 offset-xl-8 offset-lg-7">
-              <p className="hero-text wow img-custom-anim-right animated text-smoke">
-                We are digital agency that helps immersive and engaging user
-                experiences that
-              </p>
-              <Link
-                scroll={false}
-                href="/project"
-                className="btn style2 mt-40 wow img-custom-anim-right animated"
-              >
-                <span className="link-effect">
-                  <span className="effect-1">WORK WITH ME</span>
-                  <span className="effect-1">WORK WITH ME</span>
-                </span>
-              </Link>
+    <div className="hero-wrapper hero-4 shape-mockup-wrap" id="hero">
+      <div
+        className="hero-4-thumb img-custom-anim-left wow animated shape-mockup"
+        style={{ left: "0px" }}
+      >
+        <Image
+          width={838}
+          height={838}
+          className="w-100"
+          src="/assets/img/hero/hero-main.png"
+          alt="img"
+        />
+      </div>
+      <div className="bg-theme">
+        <div className="container">
+          <div className="hero-style4">
+            <div className="row justify-content-end">
+              <div className="col-lg-7">
+                <h1 className="hero-title wow img-custom-anim-right animated">
+                  Unleash Exponential Growth.
+                </h1>
+                <p className="hero-text wow img-custom-anim-right animated">
+                  Unleash a custom-built website that propels your brand, captivates visitors, and drives conversions.
+                  Our expert design and development craft seamless user experiences with the unique needs of your business at the core.
+                </p>
+                <div className="btn-group fade_right">
+                  <Link
+                    scroll={false}
+                    href="/project"
+                    className="btn wow img-custom-anim-right animated"
+                  >
+                    <span className="link-effect">
+                      <span className="effect-1">VIEW OUR WORKS</span>
+                      <span className="effect-1">VIEW OUR WORKS</span>
+                    </span>
+                  </Link>
+                </div>
+                <div className="hero-thumb-group img-custom-anim-right wow animated">
+                  <Image
+                    width={60}
+                    height={60}
+                    className="img1"
+                    src="/assets/img/hero/hero-counter1-2.png"
+                    alt="img"
+                  />
+                  <Image
+                    width={60}
+                    height={60}
+                    className="img2"
+                    src="/assets/img/hero/hero-counter1-2.png"
+                    alt="img"
+                  />
+                  <div className="hero-counter counter-item">
+                    <span className="counter-number">10</span>K
+                  </div>
+                  <p>
+                    More than <span className="counter-number">10</span>k+
+                    trusted customers
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
